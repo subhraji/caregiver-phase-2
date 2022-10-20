e49260e7051e629adb7f123cb6d775ea372c7f08
subhrajit <subhrajit.deb@ekodus.com>
2022-10-20 22:24:25 +0530

    job details page ui-2: 20-10-2022

--- app/src/main/java/com/example/caregiverphase2/ui/activity/JobDetailsActivity.kt
+++ app/src/main/java/com/example/caregiverphase2/ui/activity/JobDetailsActivity.kt
@@ -2,9 +2,12 @@ package com.example.caregiverphase2.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.caregiverphase2.R
 import com.example.caregiverphase2.databinding.ActivityAuthBinding
 import com.example.caregiverphase2.databinding.ActivityJobDetailsBinding
+import com.user.caregiver.gone
+import com.user.caregiver.visible
 
 class JobDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityJobDetailsBinding
@@ -14,8 +17,41 @@ class JobDetailsActivity : AppCompatActivity() {
         binding= ActivityJobDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        clickJobOverview()
+
         binding.backBtn.setOnClickListener {
             finish()
         }
+
+        binding.jobOverviewCard.setOnClickListener {
+            clickJobOverview()
+        }
+
+        binding.checklistCard.setOnClickListener {
+            clickCheckList()
+        }
+    }
+
+    private fun clickJobOverview(){
+        binding.jobOverviewTv.setBackgroundResource(R.color.theme_blue)
+        binding.jobOverviewTv.setTextColor(ContextCompat.getColor(this, R.color.white))
+
+        binding.checkListTv.setBackgroundResource(R.color.white)
+        binding.checkListTv.setTextColor(ContextCompat.getColor(this, R.color.theme_blue))
+
+        binding.relativeLay1.visible()
+        binding.relativeLay2.gone()
+
+    }
+
+    private fun clickCheckList(){
+        binding.checkListTv.setBackgroundResource(R.color.theme_blue)
+        binding.checkListTv.setTextColor(ContextCompat.getColor(this, R.color.white))
+
+        binding.jobOverviewTv.setBackgroundResource(R.color.white)
+        binding.jobOverviewTv.setTextColor(ContextCompat.getColor(this, R.color.theme_blue))
+
+        binding.relativeLay2.visible()
+        binding.relativeLay1.gone()
     }
 }
