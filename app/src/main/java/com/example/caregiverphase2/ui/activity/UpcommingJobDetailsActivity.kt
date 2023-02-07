685fbdc3511755817db098d89a213169ac3134d7
subhrajitdeb <subhrajitdeb54@gmail.com>
2023-02-07 17:41:52 +0530

    slider accept btn - 7/2/2023

--- app/src/main/java/com/example/caregiverphase2/ui/activity/UpcommingJobDetailsActivity.kt
+++ app/src/main/java/com/example/caregiverphase2/ui/activity/UpcommingJobDetailsActivity.kt
@@ -1,13 +1,16 @@
 package com.example.caregiverphase2.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.caregiverphase2.R
-import com.example.caregiverphase2.databinding.ActivityJobDetailsBinding
 import com.example.caregiverphase2.databinding.ActivityUpcommingJobDetailsBinding
+import com.ncorti.slidetoact.SlideToActView
 import gone
 import visible
+
 
 class UpcommingJobDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUpcommingJobDetailsBinding
@@ -22,6 +25,42 @@ class UpcommingJobDetailsActivity : AppCompatActivity() {
         binding.jobOverviewCard.setOnClickListener {
             clickJobOverview()
         }
+        binding.checklistCard.setOnClickListener {
+            clickCheckList()
+
+        }
+
+        //swipe
+        binding.slideToStartBtn.setOnClickListener {
+        }
+
+
+        binding.slideToStartBtn.onSlideCompleteListener = (object : SlideToActView.OnSlideCompleteListener{
+            override fun onSlideComplete(view: SlideToActView) {
+                binding.slideToStartBtn.resetSlider()
+            }
+        })
+
+        binding.slideToStartBtn.onSlideToActAnimationEventListener = (object : SlideToActView.OnSlideToActAnimationEventListener{
+            override fun onSlideCompleteAnimationEnded(view: SlideToActView) {
+                //Toast.makeText(this@UpcommingJobDetailsActivity,"onSlideCompleteAnimationEnded",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onSlideCompleteAnimationStarted(view: SlideToActView, threshold: Float) {
+                //Toast.makeText(this@UpcommingJobDetailsActivity,"onSlideCompleteAnimationStarted",Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onSlideResetAnimationEnded(view: SlideToActView) {
+                //Toast.makeText(this@UpcommingJobDetailsActivity,"onSlideResetAnimationEnded",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onSlideResetAnimationStarted(view: SlideToActView) {
+                Toast.makeText(this@UpcommingJobDetailsActivity,"Calling api...",Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
     }
 
     private fun clickJobOverview(){
@@ -46,4 +85,5 @@ class UpcommingJobDetailsActivity : AppCompatActivity() {
         binding.relativeLay2.visible()
         binding.relativeLay1.gone()
     }
+
 }
